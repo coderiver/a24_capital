ef97db65cc401fc13bfe828d73304674beca9724
whats0n <mrdvv@mail.ru>
2014-11-19 16:15:45 +0200

    fixed

--- js/common.js
+++ js/common.js
@@ -100,25 +100,25 @@ head.ready(function() {
 	// 	$(this).parent('.audio-player').find('.ap').prop('muted');
 	// 	$(this).toggleClass('is-active');
 	// });
-	// $(".ap").prop('muted', false);
+	$(".ap").prop('muted', false);
 
-	//  $(".audio-volume").click( function (){
-	//    if( $(".ap").prop('muted') ) {
-	//          $(".ap").prop('muted', false);
-	//    } else {
-	//      $(".ap").prop('muted', true);
-	//    }
-	//    $(this).toggleClass('is-active');
-	// });
-	var audio = document.getElementById('audio-player');
-
-	document.getElementById('mute').addEventListener('click', function (e)
-	{
-	    e = e || window.event;
-	    audio.muted = !audio.muted;
-	    e.preventDefault();
-	}, false);
-	$(".audio-volume").click( function (){
-		$(this).toggleClass('is-active');
+	 $(".audio-volume").click( function (){
+	   if( $(".ap").prop('muted') ) {
+	         $(".ap").prop('muted', false);
+	   } else {
+	     $(".ap").prop('muted', true);
+	   }
+	   $(this).toggleClass('is-active');
 	});
+	// var audio = document.getElementById('audio-player');
+
+	// document.getElementById('mute').addEventListener('click', function (e)
+	// {
+	//     e = e || window.event;
+	//     audio.muted = !audio.muted;
+	//     e.preventDefault();
+	// }, false);
+	// $(".audio-volume").click( function (){
+	// 	$(this).toggleClass('is-active');
+	// });
 });
